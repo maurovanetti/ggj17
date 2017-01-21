96cd4eed5763959d63c8f41d6ad7b74380a0af84
Andrea Jegher <andreaj-93@hotmail.it>
2017-01-21 09:47:05 +0100

    Aggiungti scripts

---
+++ Assets/Scripts/NotMyTempo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class NotMyTempo : MonoBehaviour {
+
+    //public string m_userInputKey;
+    //public AudioClip m_audioTrack;
+    public float m_startOffset = 0.0f;
+    public float m_step = 0.4545f;
+    public float m_saveTime = 0.2f;
+
+    float audioStartTime;
+    AudioSource audioPlayer;
+    float pressTime;
+    float nextKey;
+
+    void Start()
+    {
+        audioPlayer = GetComponent<AudioSource>();
+        audioPlayer.Play();
+        audioStartTime = Time.unscaledTime;
+        nextKey = audioStartTime + m_step;
+        Debug.Log("Start time:"+audioStartTime);
+        StartCoroutine(CheckTempo());
+    }
+
+    private IEnumerator CheckTempo()
+    {
+        while (true)
+        {
+            //Debug.Log("Key at :" + nextKey);
+            yield return new WaitForSecondsRealtime(m_step);
+            nextKey = Time.unscaledTime + m_step; 
+        }
+    }
+
+    void FixedUpdate () {
+        //Debug.Log("Fixed: "+Time.fixedTime);
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            pressTime = Time.unscaledTime;
+            //Debug.Log("Input in: "+ pressTime);
+            if (TimingIsCorrect())
+                Debug.Log("Timing ok:" + pressTime + " key is" +nextKey);
+            else
+                Debug.Log("Timing wrong:" + pressTime + " key is" + nextKey);
+        }
+	}
+
+    private bool TimingIsCorrect()
+    {
+       return pressTime <= nextKey + m_saveTime && pressTime >= nextKey - m_saveTime;
+    }
+}
