77c1d483218d70c73121a4b340818ba995343b78
Mauro Vanetti <posta@maurovanetti.info>
2017-01-21 11:39:57 +0100

    Esperimento NavMesh riuscito

---
+++ Assets/Scripts/GoToTarget.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GoToTarget : MonoBehaviour {
+    
+    private NavMeshAgent agent;
+    public Transform Target
+    {
+        private get;
+        set;
+    }
+
+    // Use this for initialization
+    void Start () {
+        agent = GetComponent<NavMeshAgent>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Target != null && agent.destination != Target.position)
+        {
+            agent.destination = Target.position;
+        }
+	}
+}
