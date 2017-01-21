a53455c3cfa41bfd5ccd39dcda5aa411a940a0f3
TheL94 <lu.gbelli@hotmail.it>
2017-01-21 11:57:38 +0100

    - Load Scene Async Test

--- Assets/Scripts/Utility/SceneController.cs
+++ Assets/Scripts/Utility/SceneController.cs
@@ -12,14 +12,23 @@ namespace Utility
         public UnityEvent OnStart;
          [Header("Preload Scene Params")]
         public KeyCode m_runKey;
+        public KeyCode m_escKey;
         public MaskableGraphic m_loadDoneAlert;
         public MaskableGraphic m_holdAlert;
         bool start = false;
 
+        Canvas canvas;
+        
+
         void Start()
         {
+            /*
             if (OnStart != null)
                 OnStart.Invoke();
+            */
+            PreloadScene();
+
+            canvas = GetComponentInChildren<Canvas>();
         }
 
         public void OpenScene(string sceneName)
@@ -37,6 +46,12 @@ namespace Utility
             StartCoroutine(HandlePreload(sceneName));
         }
 
+        public void PreloadScene()
+        {
+            string sceneName = "Test0";
+            StartCoroutine(HandlePreload(sceneName));
+        }
+
         public void StartGame()
         {
             start = true;
@@ -44,26 +59,30 @@ namespace Utility
 
         private IEnumerator HandlePreload(string sceneName)
         {
-            yield return new WaitForSeconds(1.0f);
+            //yield return new WaitForSeconds(1.0f);
 
             AsyncOperation loadSceneAsync = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
             loadSceneAsync.allowSceneActivation = false;
 
+            /*
             while (loadSceneAsync.progress < 0.89f)
             {
                 yield return new WaitForFixedUpdate();
             }
-
+            
             if (m_loadDoneAlert)
                 m_loadDoneAlert.enabled = true;
-
-            while (!Input.GetKeyDown(m_runKey) || start)
+            */
+            while ((!Input.GetKeyDown(m_runKey)) || (!start))
             {
                 yield return new WaitForFixedUpdate();
             }
 
+            canvas.gameObject.SetActive(false);
+            /*
             m_loadDoneAlert.enabled = false;
             m_holdAlert.enabled = true;
+            */
             loadSceneAsync.allowSceneActivation = true;
         }
     }
