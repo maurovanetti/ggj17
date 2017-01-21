8557e657c3f1fd47c04c277a0221e40fb0feba99
Mauro Vanetti <posta@maurovanetti.info>
2017-01-21 18:04:41 +0100

    File aggiunti

---
+++ Assets/Scripts/Enemies/AbstractEnemyAI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public abstract class AbstractEnemyAi : MonoBehaviour {
+    
+    public float killingRadius;
+
+    const string characterTag = "Player";
+    protected GameObject character;
+
+    // Use this for initialization
+    void Start() {
+        Initialize();
+    }
+
+    protected void Initialize() {
+        character = GameObject.FindGameObjectWithTag(characterTag);
+	}
+
+    protected void CheckKill()
+    {
+        if (CharacterDistance() <= killingRadius)
+        {
+            Debug.LogWarning(this.name + " killed " + character.name);
+            character.GetComponent<TopDownCharacter>().OnDeath();
+            GetComponent<GoToTarget>().Target = transform; // Freezes
+        }
+    }
+
+    protected float CharacterDistance()
+    {
+        if (character)
+        {
+            return Vector3.Distance(transform.position, character.transform.position);
+        }
+        else return float.PositiveInfinity;
+        
+    }
+}
---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    GameObject player;       //Public variable to store a reference to the player game object
+    public float maxOffsetMagnitude;
+    public Vector3 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate()
+    {
+        offset = transform.position - player.transform.position;
+        offset.y = 0;
+        if (offset.magnitude > maxOffsetMagnitude)
+        {
+            transform.position += offset / 2;
+        }
+    }
+}
