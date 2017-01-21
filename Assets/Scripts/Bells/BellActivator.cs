7de9bfa3f4dd70f696efff78bd1d144b96da0ce9
Andrea Jegher <andreaj-93@hotmail.it>
2017-01-22 00:50:26 +0100

    Aggiunto script attivazione campane

---
+++ Assets/Scripts/Bells/BellActivator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BellActivator : MonoBehaviour {
+
+    public float m_activationDistance = 0.2f;
+
+    GameObject player;
+    Transform tr;
+
+    NotMyTempo notMyTempoScript;
+    Bell bellScript;
+
+    bool val;
+
+    // Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        notMyTempoScript = GetComponent<NotMyTempo>();
+        bellScript = GetComponent<Bell>();
+        tr = gameObject.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        val = Vector3.Distance(player.transform.position, tr.position) <= m_activationDistance;
+        SetActiveStateToScripts(val);
+	}
+
+    private void SetActiveStateToScripts(bool val)
+    {
+        notMyTempoScript.enabled = val;
+        bellScript.enabled = val;
+    }
+}
