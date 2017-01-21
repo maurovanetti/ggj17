ba213412d8d9367c219a77a9798df5cbffc34b36
TheL94 <lu.gbelli@hotmail.it>
2017-01-21 23:48:50 +0100

    - aggiunta file testi - prima bozza SpriteLayerPerspectiveSimulator

---
+++ Assets/Scripts/Utility/SpriteLayerPerspectiveSimulator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteLayerPerspectiveSimulator : MonoBehaviour
+{
+    Transform objTrasform;
+
+    void Start()
+    {
+        objTrasform = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        //TranslateOnY();
+    }
+
+    void TranslateOnY()
+    {
+        objTrasform.position = new Vector3(objTrasform.position.x, objTrasform.position.y - (objTrasform.position.z / 10000), objTrasform.position.z);
+    }
+}
